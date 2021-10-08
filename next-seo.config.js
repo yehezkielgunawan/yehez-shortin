f3f5394341d30d20db16630c1226ee10fd06aa12
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-10-08 17:00:02 +0700

    style(next-seo.config): :art: update next-seo (og image)

--- next-seo.config.js
+++ next-seo.config.js
@@ -12,7 +12,7 @@ const defaultSEOConfig = {
     type: "website",
     images: [
       {
-        url: "https://yehez-og-image.yehezgun.com/**link**.yehezgun.com.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1631970666%2Fyehez_avatar_vkv7pc.png&widths=200&heights=200",
+        url: "https://yehez-og-image.yehezgun.com/**link**.yehezgun.com.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1631970666%2Fyehez_avatar_vkv7pc.png&widths=200&heights=200",
         alt: "link.yehezgun.com og-image",
         width: 800,
         height: 600,
