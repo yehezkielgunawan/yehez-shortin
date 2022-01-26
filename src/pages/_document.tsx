b951145914ec55eb3a30760c7f74a2c10271f559
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-01-26 19:29:34 +0700

    style(emotion-cache and _document): :art: prevent FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,36 @@
 import { ColorModeScript } from "@chakra-ui/react";
+import createEmotionServer from "@emotion/server/create-instance";
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import React from "react";
 
-export default class Document extends NextDocument {
+import emotionCache from "@/functions/lib/emotion-cache";
+
+const { extractCritical } = createEmotionServer(emotionCache);
+
+export default class CustomDocument extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
+
   render() {
     return (
       <Html>
